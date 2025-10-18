79cd065e32d6b2b4b6621b4f760448ebc109718c
Sriya Rallabandi <77068737+sriya26@users.noreply.github.com>
2025-10-18 18:46:51 -0400

    Time: 0 ms (100%), Space: 17.1 MB (45.6%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        queue<pair<pair<int,int>, int>> q;
+        //{{r,c},t}
+        int cntFresh=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    visited[i][j] = 1;
+                }
+                else{
+                    visited[i][j]=0;
+                }
+                if(grid[i][j]==1) cntFresh++;
+            }
+        }
+
+        int tm=0;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        int cnt=0;
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c= q.front().first.second;
+            int t=q.front().second;
+            tm=max(tm, t);
+            q.pop();
+            for(int i=0;i<4;i++){
+                    int nrow=r+drow[i];
+                    int ncol=c+dcol[i];
+
+                    if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
+    && visited[nrow][ncol]!=2 && grid[nrow][ncol] == 1){
+                        q.push({{nrow,ncol}, t+1});
+                        visited[nrow][ncol]=2;
+                        cnt++;
+                    }
+
+            }
+        }
+
+        if(cnt!=cntFresh) return -1;
+
+        return tm;
+        
+    }
+};
